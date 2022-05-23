607440009504390a7a3938eb20b4a0afd46703bf
Nathaniel Angus <nangus@Nathaniels-MacBook-Pro.local>
2022-05-22 23:43:07 -0400

    Room model created. Model serializer created

--- team_tunes_backend/api/urls.py
+++ team_tunes_backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('', main)
+    path('room', RoomView.as_view()),
+    path('', RoomView.as_view())
 ]
